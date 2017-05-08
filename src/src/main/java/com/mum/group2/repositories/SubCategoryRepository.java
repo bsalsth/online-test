189873782612e94f21d9da69b3cc42d48555806e
984744 <984744@cslab-v32-206a.cs.mum.edu>
2017-05-08 10:37:58 -0500

    2017-05-08 for Loading Questions file

--- src/src/main/java/com/mum/group2/repositories/SubCategoryRepository.java
+++ src/src/main/java/com/mum/group2/repositories/SubCategoryRepository.java
@@ -1,13 +1,8 @@
 package com.mum.group2.repositories;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.mum.group2.domain.Question;
 import com.mum.group2.domain.SubCategory;
 
 @Repository
@@ -18,4 +13,7 @@ public interface SubCategoryRepository extends JpaRepository<SubCategory, Intege
 //	//@Query(value = "SELECT q.* FROM question q, sub_category sc, QUESTION_SUBCAT qs where sc.sub_cat_id = :subCatId and q.question_id = qs.question_id and sc.sub_cat_id = qs.sub_cat_id ORDER BY RAND() LIMIT :numQuestionn", nativeQuery = true)
 //	@Query("SELECT q FROM ")
 //	public List<Question> getFirstNQuestion(@Param("subCatId") int subCatId, @Param("numQuestionn") int numQuestionn);
+	
+	public SubCategory findByDescription(String description);
+	
 }
